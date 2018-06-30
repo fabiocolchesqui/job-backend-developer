3aa8d727c17d99a9c7c3d42fdb96766869cdae5d
fabio <fabio@192.168.0.11>
2018-06-29 23:11:11 -0300

    Adicionado a classe entity - Credential

---
+++ src/main/java/br/com/intelipost/entity/Credential.java
@@ -0,0 +1,81 @@
+package br.com.intelipost.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * The Class Credential Entity.
+ */
+@Entity
+@Table(name = "credential")
+public class Credential {
+
+	/** Integer id. */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	/** The login. */
+	private String login;
+	
+	/** The password. */
+	private String password;
+
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the login.
+	 *
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * Sets the login.
+	 *
+	 * @param login the new login
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
+	 * Gets the password.
+	 *
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * Sets the password.
+	 *
+	 * @param password the new password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
