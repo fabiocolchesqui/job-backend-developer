ae2617c621d45757f2fec3b5c2dd9dd4fdbac03f
fabio <fabio@192.168.0.11>
2018-06-29 23:14:59 -0300

    Adicionado os arquivos de configuração da aplicação, spring security e o script sql default para o banco de dados H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into credential values(1001, 'login1', '1234');
+insert into credential values(1002, 'login2', '1234');
+insert into credential values(1003, 'login3', '1234');
+insert into credential values(1004, 'login4', '1234');
+insert into credential values(1005, 'login5', '1234');
